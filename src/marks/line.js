cfae96469f47a404c3e28866a3e2253a99b1543d
lukas <lukashermann28@gmail.com>
2021-04-14 17:51:41 -0400

    Feat: markline

--- src/marks/line.js
+++ src/marks/line.js
@@ -8,40 +8,67 @@ import {
 } from 'twgl.js/dist/4.x/twgl-full.module.js';
 
 const vs = `
-    attribute vec2 position;
-    attribute vec3 color;
-    uniform vec2 resolution;
-    uniform vec2 origin;
-    varying vec3 fill;
+precision mediump float;
+uniform vec2 resolution;
+uniform vec2 origin;
 
-    void main() {
-      fill = color;
-      vec2 pos = position + origin;
-      pos /= resolution;
-      pos.y = 1.0-pos.y;
-      pos = pos*2.0-1.0;
-      gl_Position = vec4(pos, 0, 1);
-    }
+attribute vec2 position;
+attribute vec2 normal;
+
+void main() {
+	vec2 pos = (position + origin) / resolution;
+	pos += normal * 1.0 / resolution;
+	pos.y = 1.0 - pos.y;
+	pos = pos * 2.0 - 1.0;
+	gl_Position = vec4(pos, 0, 1);
+}
 `;
 
 const fs = `
-    precision mediump float;
-    varying vec3 fill;
-    void main() {
-      gl_FragColor = vec4(fill.xyz, 1);
-    }
+precision mediump float;
+uniform vec3 stroke;
+
+void main() {
+	gl_FragColor = vec4(stroke, 1);
+}
 `;
 
 function draw(gl, item) {
-  const itemCount = item.items.length;
   const positions = [];
+  const normals = [];
   const colors = [];
-  for (let i = 0; i < itemCount; i++) {
-    const {x, y, stroke, strokeWidth: sw} = item.items[i];
-    const {x: x2, y: y2} = item.items[i + 1 < itemCount ? i + 1 : i];
-    positions.push(x, y, 0, x + sw, y, 0, x2, y2, 0, x2, y2, 0, x2 + sw, y2, 0, x + sw, y, 0);
+
+  for (let i = 0; i < item.items.length - 1; i++) {
+    const {x, y, stroke} = item.items[i];
+    const {x: x2, y: y2} = item.items[i + 1];
+    const [dx, dy] = [x2 - x, y2 - y];
+    let [nx, ny] = [-dy, dx];
+    const vlen = Math.sqrt(nx ** 2 + ny ** 2);
+    nx /= vlen;
+    ny /= vlen;
+    positions.push(x, y, 0, x, y, 0, x2, y2, 0, x2, y2, 0, x2, y2, 0, x, y, 0);
+    normals.push(nx, ny, 0, -nx, -ny, 0, -nx, -ny, 0, -nx, -ny, 0, nx, ny, 0, nx, ny, 0);
     const col = color(stroke);
-    colors.push(col.r / 255, col.g / 255, col.b / 255);
+    colors.push(
+      col.r / 255,
+      col.g / 255,
+      col.b / 255,
+      col.r / 255,
+      col.g / 255,
+      col.b / 255,
+      col.r / 255,
+      col.g / 255,
+      col.b / 255,
+      col.r / 255,
+      col.g / 255,
+      col.b / 255,
+      col.r / 255,
+      col.g / 255,
+      col.b / 255,
+      col.r / 255,
+      col.g / 255,
+      col.b / 255
+    );
   }
 
   const programInfo = createProgramInfo(gl, [vs, fs]);
@@ -49,17 +76,18 @@ function draw(gl, item) {
 
   const buffer = {
     position: {data: positions},
+    normal: {data: normals},
     color: {data: colors, numComponents: 3}
+    // strokewidth: {data: strokewidths, numComponents: 1}
   };
 
   setUniforms(programInfo, this._uniforms);
   const bufferInfo = createBufferInfoFromArrays(gl, buffer);
-  bufferInfo.numElements = itemCount;
   setBuffersAndAttributes(gl, programInfo, bufferInfo);
   drawBufferInfo(gl, bufferInfo, gl.TRIANGLE);
 }
 
 export default {
   type: 'line',
-  draw: draw,
+  draw: draw
 };
