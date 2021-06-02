fecd9765710b1d95adc87efe3fae6bc5cacd0790
lukas <lukashermann28@gmail.com>
2021-06-02 15:55:49 -0700

    fixing colors on lines

--- src/marks/line.js
+++ src/marks/line.js
@@ -28,16 +28,17 @@ void main() {
 
 const fs = `
 precision mediump float;
+uniform vec3 stroke;
 
 void main() {
-	gl_FragColor = vec4(1, 0, 1, 1);
+	gl_FragColor = vec4(stroke, 1);
 }
 `;
 
 function draw(gl, item, tfx) {
   const positions = [];
   const normals = [];
-  const colors = [];
+  // const colors = [];
   const strokewidths = [];
 
   for (let i = 0; i < item.items.length - 1; i++) {
@@ -50,30 +51,10 @@ function draw(gl, item, tfx) {
     ny /= vlen;
     positions.push(x, y, 0, x, y, 0, x2, y2, 0, x2, y2, 0, x2, y2, 0, x, y, 0);
     normals.push(nx, ny, 0, -nx, -ny, 0, -nx, -ny, 0, -nx, -ny, 0, nx, ny, 0, nx, ny, 0);
-    const col = color(stroke);
-    colors.push(
-      col.r / 255,
-      col.g / 255,
-      col.b / 255,
-      col.r / 255,
-      col.g / 255,
-      col.b / 255,
-      col.r / 255,
-      col.g / 255,
-      col.b / 255,
-      col.r / 255,
-      col.g / 255,
-      col.b / 255,
-      col.r / 255,
-      col.g / 255,
-      col.b / 255,
-      col.r / 255,
-      col.g / 255,
-      col.b / 255
-    );
     const sw = strokeWidth || 1;
     strokewidths.push(sw, sw, sw, sw, sw, sw);
   }
+  const col = color(item.items[0].stroke);
 
   const programInfo = createProgramInfo(gl, [vs, fs]);
   gl.useProgram(programInfo.program);
@@ -81,11 +62,10 @@ function draw(gl, item, tfx) {
   const buffer = {
     position: {data: positions},
     normal: {data: normals},
-    color: {data: colors, numComponents: 3},
     strokewidth: {data: strokewidths, numComponents: 1}
   };
 
-  setUniforms(programInfo, {...this._uniforms, origin: tfx});
+  setUniforms(programInfo, {...this._uniforms, origin: tfx, stroke: [col.r / 255.0, col.g / 255.0, col.b / 255.0]});
   const bufferInfo = createBufferInfoFromArrays(gl, buffer);
   setBuffersAndAttributes(gl, programInfo, bufferInfo);
   drawBufferInfo(gl, bufferInfo, gl.TRIANGLE);
